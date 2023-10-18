871e01f2fe28a3bd53943e88242a7e7d54f9e3b5
kyohei328 <zwei328@gmail.com>
2023-10-18 12:40:46 +0900

    Appjs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { BrowserRouter,Routes,Route, Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import styled from 'styled-components'
 import Login from './components/Login';
 // import IndexPhoto from './components/IndexPhoto';
 // import AddPhoto from './components/AddPhoto';
 import UserCreate from './components/UserCreate';
+import Top from './components/Top';
 
-function App() {
 
+
+const Logo = styled.div`
+  font-weight: bold;
+  font-size: 23px;
+  letter-spacing: 3px;
+`
 const Nabvar = styled.nav`
   background: #dcdcdc;
   min-height: 6vh;
@@ -16,13 +22,6 @@ const Nabvar = styled.nav`
   justify-content: space-around;
   align-items: center;
 `
-
-const Logo = styled.div`
-  font-weight: bold;
-  font-size: 23px;
-  letter-spacing: 3px;
-`
-
 const NavItems = styled.ul`
   display: flex;
   width: 400px;
@@ -30,7 +29,6 @@ const NavItems = styled.ul`
   justify-content: space-around;
   list-style: none;
 `
-
 const NavItem = styled.li`
   font-size: 19px;
   font-weight: bold;
@@ -50,39 +48,54 @@ const StyledLink = styled(Link)`
   color: #000000;
 `
 
+function App(props) {
+  const [current_user, setCurrent_user] = useState('');
+
+  const current_userChange = (user) => {
+    setCurrent_user(user);
+  };
+
+  console.log(current_user)
+
   return (
-    <BrowserRouter>
+    <>
       <Nabvar>
         <Logo>
-          PhotoSample
+          <StyledLink to="/">
+            PhotoSample
+          </StyledLink>
         </Logo>
         <NavItems>
           <NavItem>
-            <StyledLink to="/photo/new">
+            {/* <StyledLink to="/photo/new">
               アップロード
             </StyledLink>
           </NavItem>
           <NavItem>
             <StyledLink to="/photo/index">
               写真一覧
-            </StyledLink>
+            </StyledLink> */}
           </NavItem>
           <NavItem>
             <StyledLink to="/Login">
               ログイン
             </StyledLink>
           </NavItem>
+          <NavItem>
+            {current_user}
+          </NavItem>
         </NavItems>
       </Nabvar>
       <Wrapper>
         <Routes>
-          <Route path="/photo/index" element={<IndexPhoto />}/>
-          <Route path="/photo/new" element={<AddPhoto />}/>
-          <Route path="/Login" element={<Login />}/>
+          <Route path="/" element={<Top />}/>
+          {/* <Route path="/photo/index" element={<IndexPhoto />}/>
+          <Route path="/photo/new" element={<AddPhoto />}/> */}
+          <Route path="/Login" element={<Login onUserChange={current_userChange}/>}/>
           <Route path="/user/new" element={<UserCreate />}/>
         </Routes>
       </Wrapper>
-    </BrowserRouter>
+    </>
   );
 }
 
